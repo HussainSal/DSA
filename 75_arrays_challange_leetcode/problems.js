6b69d1f58ea2549cd0a8bb2c22ad40cd20075c45
HussainSal <salmanmian54321@gmail.com>
2024-11-03 12:22:49 +0530

    problem 2 :- 75 day challange problem solved

--- 75_arrays_challange_leetcode/problems.js
+++ 75_arrays_challange_leetcode/problems.js
@@ -27,3 +27,58 @@ function mergeAlternately(word1,word2){
 
 // console.log(mergeAlternately("abcd","pq"),"MERGE_ALTERNATIVELY")
 
+
+/* 
+238. Product of Array Except Self
+
+solution :- 
+
+*/
+
+function productArray (nums){
+    console.log(nums,"NUMSS")
+    let products = []
+    for(let i =0;i<nums.length;i++){
+        let temp = 1;
+        for(let j = 0; j < nums.length ;j++){
+            console.log(temp,"TEMPPP")
+            if(i != j){
+               temp = temp * nums[j];
+            }
+            console.log(temp,"TEMPPP_OUTSIDE")
+        }
+        products[i] = temp;   
+    }
+    return products
+}
+
+// optimizied silution 
+
+function productArray(nums) {
+    const n = nums.length;
+    const leftProducts = new Array(n);
+    const rightProducts = new Array(n);
+    const products = new Array(n);
+
+    // Step 1: Calculate left products
+    leftProducts[0] = 1; // There are no elements to the left of the first element
+    for (let i = 1; i < n; i++) {
+        leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+    }
+
+    // Step 2: Calculate right products
+    rightProducts[n - 1] = 1; // There are no elements to the right of the last element
+    for (let j = n - 2; j >= 0; j--) {
+        rightProducts[j] = rightProducts[j + 1] * nums[j + 1];
+    }
+
+    // Step 3: Calculate the result
+    for (let k = 0; k < n; k++) {
+        products[k] = leftProducts[k] * rightProducts[k];
+    }
+
+    return products;
+}
+
+
+console.log(productArray([-1,1,0,-3,3]),"PRODUCT_ARRAY")
